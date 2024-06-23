69b2ccf624230f17f536cd92a10a83a18efa5a19
Darko Živić <zivic.darko79@gmail.com>
2024-06-23 17:34:21 +0200

    "Updated InfoBanner component: changed icon colors, added Link component for email, and rearranged some elements"

--- frontend/src/components/home/InfoBanner.jsx
+++ frontend/src/components/home/InfoBanner.jsx
@@ -2,38 +2,41 @@ import React from 'react'
 import { RiCustomerService2Line } from "react-icons/ri";
 import { FiPhoneCall, FiMail } from "react-icons/fi";
 import { TbTruckDelivery } from "react-icons/tb";
+import Link from 'next/link';
 
 const InfoBanner = () => {
   return (
-    <div className="absolute h-20 bg-transparent left-1/2 -bottom-10 transform -translate-x-1/2 hidden lg:inline-flex items-center gap-x-12 pt-36 mt-32">
+    <div className="absolute h-20 bg-transparent left-1/2 -bottom-10 transform -translate-x-1/2 hidden lg:inline-flex items-center gap-x-12 pt-36 mt-32 text-gray">
     <div className="flex items-center gap-5 w-60">
-      <RiCustomerService2Line className="text-designColor w-8 h-8" />
+      <RiCustomerService2Line className="text-accent w-8 h-8" />
       <div>
         <p>Ponedjeljak - Petak</p>
         <p className="font-semibold">7:30 - 19:30</p>
       </div>
     </div>
     <div className="flex items-center gap-5 w-60">
-      <FiPhoneCall className="text-designColor w-8 h-8" />
+      <FiPhoneCall className="text-accent w-8 h-8" />
       <div>
+      <p className="font-semibold">Pozovite nas</p>
         <p>+ 385 32 442 992</p>
-        <p className="font-semibold">Pozovite nas</p>
       </div>
     </div>
     <div className="flex items-center gap-5 w-60">
-      <TbTruckDelivery className="text-designColor w-8 h-8" />
+      <TbTruckDelivery className="text-accent w-8 h-8" />
       <div>
         <p>Dostava za sve</p>
         <p className="font-semibold"> iznose 4,00€</p>
       </div>
     </div>
-    <div className="flex items-center gap-5 w-60">
-      <FiMail className="text-designColor w-8 h-8" />
+    <Link
+    href={'mailto:prodaja@zivic-elektro.com'} target='blank'
+    className="flex items-center gap-5 w-60">
+      <FiMail className="text-accent w-8 h-8" />
       <div>
+      <p className="font-semibold">Email</p>
         <p>Pošaljite upit</p>
-        <p className="font-semibold">Email</p>
       </div>
-    </div>
+    </Link>
   </div>
   )
 }
