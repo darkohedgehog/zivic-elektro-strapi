336877b2822c44eb3282b4fa4efaa30f5897dceb
Darko Živić <zivic.darko79@gmail.com>
2024-07-08 20:13:34 +0200

    "Added generateMetadata function and wrapped UserOrders component in a React fragment"

--- frontend/src/app/orders/page.jsx
+++ frontend/src/app/orders/page.jsx
@@ -1,7 +1,27 @@
 import UserOrders from '@/components/orders/UserOrders';
+import React from 'react'
+
+export async function generateMetadata() {
+  return {
+    title: "Vaše narudžbe",
+    description: "Ovdje možete vidjeti vaše narudžbe",
+    robots: {
+      index: false,
+      follow: false,
+      googleBot: {
+        index: false,
+        follow: false,
+      },
+    },
+  };
+}
 
 const OrdersPage = () => {
-  return <UserOrders />;
+  return (
+    <>
+    <UserOrders />;
+    </>
+  )
 };
 
 export default OrdersPage;
