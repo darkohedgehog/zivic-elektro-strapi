31702253988af3d3744084f785ad3a2252bee438
Darko Živić <zivic.darko79@gmail.com>
2024-07-09 22:49:35 +0200

    Added nodemailer instead Resend

--- frontend/src/app/api/send-email/route.js
+++ frontend/src/app/api/send-email/route.js
@@ -1,21 +1,51 @@
+import nodemailer from 'nodemailer';
+import { render } from '@react-email/render';
 import OrderConfirmationEmail from '@/components/email/OrderConfirmationEmail';
-import { Resend } from 'resend';
 
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+
+
+// Konfiguracija SMTP transporta
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: true, 
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
 export async function POST(req) {
   try {
     const { orderData } = await req.json();
 
-    const emailResponse = await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: orderData.email,
-      subject: 'Order Confirmation',
-      react: <OrderConfirmationEmail orderData={orderData} />,
-    });
+    // Renderuj komponentu u HTML string
+    const emailHtml = render(<OrderConfirmationEmail orderData={orderData} />);
 
-    return new Response(JSON.stringify({ success: true, data: emailResponse }), {
+    const mailOptionsPrimary = {
+      from: process.env.SMTP_USER,
+      to: orderData.email,
+      subject: 'Potvrda narudžbe',
+      html: emailHtml,
+    };
+
+    const mailOptionsSecondary = {
+      from: process.env.SMTP_USER,
+      to: 'prodaja@zivic-elektro.com',
+      subject: 'Nova narudžba',
+      html: emailHtml,
+    };
+
+    // Slanje na primarnu adresu
+    await transporter.sendMail(mailOptionsPrimary);
+    console.log('Email sent to primary address');
+
+    // Slanje na sekundarnu adresu
+    await transporter.sendMail(mailOptionsSecondary);
+    console.log('Email sent to secondary address');
+
+    return new Response(JSON.stringify({ success: true }), {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
