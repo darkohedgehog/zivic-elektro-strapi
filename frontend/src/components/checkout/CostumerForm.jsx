7ce0b7e9bef2b35d7948305de912fd59651e3860
Darko Živić <zivic.darko79@gmail.com>
2024-07-10 18:30:43 +0200

    "Updated sitemap XML file and added 'required' attribute to input fields in CostumerForm.jsx"

--- frontend/src/components/checkout/CostumerForm.jsx
+++ frontend/src/components/checkout/CostumerForm.jsx
@@ -6,34 +6,37 @@ const CostumerForm = ({orderData, handleChange}) => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-3 my-4">
           <div>
             <label className="block text-sm font-medium text-gray-700">Vaše ime</label>
-            <input type="text" name="firstName" value={orderData.firstName} onChange={handleChange} className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
+            <input type="text" name="firstName" value={orderData.firstName} onChange={handleChange}required className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
           </div>
           <div>
             <label className="block text-sm font-medium text-gray-700">Vaše prezime</label>
-            <input type="text" name="lastName" value={orderData.lastName} onChange={handleChange} className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
+            <input type="text" name="lastName" value={orderData.lastName} onChange={handleChange} required className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
           </div>
           <div>
             <label className="block text-sm font-medium text-gray-700">Email</label>
-            <input type="email" name="email" value={orderData.email} onChange={handleChange} className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
+            <input type="email" name="email" value={orderData.email} onChange={handleChange} required
+             className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
           </div>
           <div>
             <label className="block text-sm font-medium text-gray-700">Broj mobitela</label>
-            <input type="text" name="phoneNumber" value={orderData.phoneNumber} onChange={handleChange} className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
+            <input type="text" name="phoneNumber" value={orderData.phoneNumber} onChange={handleChange} required className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
           </div>
           <div>
             <label className="block text-sm font-medium text-gray-700">Adresa za naplatu</label>
-            <input type="text" name="billingAddress" value={orderData.billingAddress} onChange={handleChange} className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
+            <input type="text" name="billingAddress" value={orderData.billingAddress} onChange={handleChange} required
+            className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
           </div>
           <div>
             <label className="block text-sm font-medium text-gray-700">Adresa za dostavu</label>
-            <input type="text" name="shippingAddress" value={orderData.shippingAddress} onChange={handleChange} className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
+            <input type="text" name="shippingAddress" value={orderData.shippingAddress} onChange={handleChange} required
+            className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-700">Ime tvrtke (Optional)</label>
+            <label className="block text-sm font-medium text-gray-700">Ime tvrtke (Ukoliko treba R-1)</label>
             <input type="text" name="companyName" value={orderData.companyName} onChange={handleChange} className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-700">OIB tvrtke (Optional)</label>
+            <label className="block text-sm font-medium text-gray-700">OIB tvrtke (Ukoliko treba R-1)</label>
             <input type="text" name="taxID" value={orderData.taxID} onChange={handleChange} 
             className="mt-1 block w-full border border-accent dark:border-accentDark rounded-lg shadow-md z-20" />
           </div>
