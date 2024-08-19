c5ac960a3575f7914a3222e43e53c119df931a5e
Darko Živić <zivic.darko79@gmail.com>
2024-08-19 21:51:20 +0200

    "Update apiUrl in GlobalApi.js from 'http://localhost:1337/api' to 'https://zivic-elektro.shop:1337/api'"

--- frontend/src/app/utils/GlobalApi.js
+++ frontend/src/app/utils/GlobalApi.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337/api';
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://zivic-elektro.shop:1337/api';
 
 const axiosClient = axios.create({
     baseURL: apiUrl,
