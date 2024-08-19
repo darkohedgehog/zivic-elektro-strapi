659cf604f2a084430d83e34423ca793209fd1438
Darko Živić <zivic.darko79@gmail.com>
2024-08-19 21:56:21 +0200

    "Updated remotePatterns in next.config.mjs"

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,20 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-          {
-            protocol: "http",
-            hostname: "localhost",
-          },
-          {
-            protocol: "https",
-            hostname: "res.cloudinary.com",
-            pathname: "/dhkmlqg4o/**",
-          },
-          
-        ],
-      },
+  reactStrictMode: true,
+  images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "zivic-elektro.shop",
+        },
+        {
+          protocol: "https",
+          hostname: "res.cloudinary.com",
+          pathname: "/dhkmlqg4o/**",
+        },
+      ],
+    },
 };
 
 export default nextConfig;
