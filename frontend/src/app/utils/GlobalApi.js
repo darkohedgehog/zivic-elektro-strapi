d86d7945e4e14d9ff81eb0e849e3e732753edcf8
Darko Živić <zivic.darko79@gmail.com>
2024-09-13 22:29:07 +0200

    "Refactored GlobalApi.js: reformatted code, removed unnecessary lines, and updated API endpoint URLs"

--- frontend/src/app/utils/GlobalApi.js
+++ frontend/src/app/utils/GlobalApi.js
@@ -3,7 +3,6 @@ const { default: axios } = require("axios");
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://api.zivic-elektro.shop/api';
 
-
 const axiosClient = axios.create({
     baseURL: apiUrl,
     headers: {
@@ -11,111 +10,109 @@ const axiosClient = axios.create({
     }
 });
 
+// Get latest products
 const getLatestProducts = () => axiosClient.get('/products?populate=*');
+
+// Get latest categories
 const getLatestCategories = () => axiosClient.get('/categories?populate=*');
-const getProductById = (id) => axiosClient.get('/products/'+id+'?populate=*');
 
-//Get product list by category
-const getProductListByCategory = (category) => axiosClient.get('/products?filters[category][$eq]='+category+"&populate=*");
+// Get product by ID
+const getProductById = (id) => axiosClient.get(`/products/${id}?populate=*`);
 
+// Get product list by category
+const getProductListByCategory = (category) => axiosClient.get(`/products?filters[category][$eq]=${category}&populate=*`);
+
+// Get product list by category name with pagination
 const getProductListByCategoryName = async (categoryName, limit = 24, start = 0) => {
-    const url = `/products?filters[category][$eq]=${categoryName}&_limit=${limit}&_start=${start}&populate=*`;
+    const url = `/products?filters[category][$eq]=${categoryName}&pagination[pageSize]=${limit}&pagination[page]=${start}&populate=*`;
     return await axiosClient.get(url);
 };
 
-
-// Get subCategory
-
+// Get subcategories by category
 const getSubCategoriesByCategory = (categoryName) => {
     const url = `/subcategories?filters[cate][$eq]=${categoryName}&populate=*`;
-   // console.log(`Fetching subcategories from URL: ${url}`); 
     return axiosClient.get(url).catch(error => {
-        console.error("API call error:", error.response); // Logovanje odgovora sa greškom
+        console.error("API call error:", error.response);
         throw error;
     });
-}
+};
 
+// Get product list by category and subcategory
 const getProductListByCategoryAndSubCategory = async (categoryName, subCategoryName) => {
     const url = `/products?filters[category][$eq]=${categoryName}&filters[subCate][$eq]=${subCategoryName}&populate=*`;
-    //console.log(`Fetching products from URL: ${url}`);
     try {
         return await axiosClient.get(url);
     } catch (error) {
-        console.error("API call error:", error.response); // Logovanje odgovora sa greškom
+        console.error("API call error:", error.response);
         throw error;
     }
-}
+};
 
-//Add bestsellers products
-
+// Get best sellers
 const getBestSellers = async () => {
     const url = `/products?filters[bestSeller][$eq]=true&populate=*`;
-   // console.log(`Fetching bestsellers from URL: ${url}`); 
     try {
         return await axiosClient.get(url);
     } catch (error) {
-        console.error("API call error:", error.response); // Logovanje odgovora sa greškom
+        console.error("API call error:", error.response);
         throw error;
     }
-}
+};
 
-//Add slug for SEO
+// Get product by slug for SEO
 const getProductBySlug = (slug) => axiosClient.get(`/products?filters[slug][$eq]=${slug}&populate=*`);
 
-//Add to Cart Collection
+// Add to cart
 const addToCart = (data) => axiosClient.post('/carts', data);
 
-//Get User Cart Items
-const getUserCartItems = (email) => axiosClient.get('/carts?populate[products][populate][0]=gallery&filters[email][$eq]='+email);
+// Get user cart items
+const getUserCartItems = (email) => axiosClient.get(`/carts?populate[products][populate][0]=gallery&filters[email][$eq]=${email}`);
 
-//Delete Cart Items
-const deleteCartItem = (id) => axiosClient.delete('/carts/' + id);
+// Delete cart item
+const deleteCartItem = (id) => axiosClient.delete(`/carts/${id}`);
 
-// Update Cart Item
-const updateCartItem = (id, data) => axiosClient.put('/carts/' + id, { data });
+// Update cart item
+const updateCartItem = (id, data) => axiosClient.put(`/carts/${id}`, { data });
 
-//Orders
+// Create order
 const createOrder = (data) => axiosClient.post('/orders', { data });
 
-
-//Clear Cart
+// Clear cart
 const clearCart = async () => {
     const cartItems = await axiosClient.get('/carts');
     const deleteRequests = cartItems.data.data.map(item => deleteCartItem(item.id));
     await Promise.all(deleteRequests);
-  };
+};
 
-  //Get User Orders
-  const getUserOrders = (email) => {
-    return axiosClient.get(`/orders?filters[email][$eq]=${email}&populate[products][populate]=gallery&populate[productQuantities][populate]=product`);
-  };
+// Get user orders
+const getUserOrders = (email) => axiosClient.get(`/orders?filters[email][$eq]=${email}&populate[products][populate]=gallery&populate[productQuantities][populate]=product`);
 
-  // Blog
-  const getBlogs = () => axiosClient.get('/blogs?populate=*');
-  const getBlogBySlug = (slug) => axiosClient.get(`/blogs?filters[slug][$eq]=${slug}&populate=*`);
+// Blog
+const getBlogs = () => axiosClient.get('/blogs?populate=*');
+const getBlogBySlug = (slug) => axiosClient.get(`/blogs?filters[slug][$eq]=${slug}&populate=*`);
 
-  const getProducts = async (limit = 24, start = 0) => {
-    const url = `/products?_limit=${limit}&_start=${start}&populate=*`;
+// Get products with pagination
+const getProducts = async (limit = 24, start = 0) => {
+    const url = `/products?pagination[pageSize]=${limit}&pagination[page]=${start}&populate=*`;
     return await axiosClient.get(url);
 };
-  
 
 export default {
     getLatestProducts,
     getLatestCategories,
     getProductById,
     getProductListByCategory,
+    getProductListByCategoryName,
+    getSubCategoriesByCategory,
+    getProductListByCategoryAndSubCategory,
+    getBestSellers,
+    getProductBySlug,
     addToCart,
     getUserCartItems,
     deleteCartItem,
     updateCartItem,
     createOrder,
     clearCart,
-    getProductListByCategoryName,
-    getSubCategoriesByCategory,
-    getProductListByCategoryAndSubCategory,
-    getBestSellers,
-    getProductBySlug,
     getUserOrders,
     getBlogs,
     getBlogBySlug,
